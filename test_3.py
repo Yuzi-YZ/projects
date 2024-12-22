b706682b9873f8dea50119f247475b08bc8e6e6c
Yuzi-YZ <yuly.tsezar.yz@gmail.com>
2024-12-22 17:21:33 +0300

    Add files via upload

---
+++ test_3.py
@@ -0,0 +1,9 @@
+import random
+def gates():
+    n = [3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+    field_1 = random.choice(n)
+    return field_1
+    # for i in range(3, 20):
+    #     if
+field_1 = gates()
+print(field_1)
